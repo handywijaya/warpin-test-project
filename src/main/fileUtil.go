0120c02b0a23e3e4f8afd5c7de0e4eaf1b814717
Handy Wijaya <handy.wijaya.p@gmail.com>
2020-05-20 17:13:03 +0700

    error handling, flush content api

--- src/main/fileUtil.go
+++ src/main/fileUtil.go
@@ -14,15 +14,30 @@ func getFilePath(fileName string) string {
 	return getBaseDirectory() + "/resources/" + fileName
 }
 
-func appendToFile(filePath string, msg string) {
+func appendToFile(filePath string, msg string) error {
 	f, _ := os.OpenFile(filePath, os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
 
-	f.WriteString(msg + "\n")
-	f.Close()
+	_, err := f.WriteString(msg + "\n")
+	if err != nil {
+		return err
+	}
+
+	err = f.Close()
+	if err != nil {
+		return err
+	}
+
+	// return no error
+	return nil
 }
 
 func readFileContent(filePath string) string {
 	content, _ := ioutil.ReadFile(filePath)
 
 	return string(content)
+}
+
+func flushFileContent(filePath string) error {
+	_, err := os.Create(getFilePath(dataFileName))
+	return err
 }
--- src/main/main.go
+++ src/main/main.go
@@ -11,6 +11,7 @@ var mutex = &sync.Mutex{}
 func main() {
 	http.HandleFunc("/send", send)
 	http.HandleFunc("/get", get)
+	http.HandleFunc("/flush", flush)
 	http.HandleFunc("/try", try)
 
 	http.ListenAndServe(":8090", nil)
@@ -19,24 +20,59 @@ func main() {
 // This API will save every messages sent with key "msg"
 // and store them into a file
 func send(w http.ResponseWriter, req *http.Request) {
-	// use mutex
 	msg := req.URL.Query()["msg"][0]
 
+	// use mutex to prevent race condition
 	mutex.Lock()
-	appendToFile(getFilePath(dataFileName), msg)
+	err := appendToFile(getFilePath(dataFileName), msg)
 	mutex.Unlock()
 
-	fmt.Fprintf(w, "OK\n")
+	// error checking after operation
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	// return response with error checking
+	_, err = fmt.Fprintf(w, "Data received!\nUse /get API to retrieve all the messages sent.")
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+	}
 }
 
 // This API will read file where the message sent is stored
 // and then print the content
 func get(w http.ResponseWriter, req *http.Request) {
+	// use mutex to prevent race condition
 	mutex.Lock()
 	content := readFileContent(getFilePath(dataFileName))
 	mutex.Unlock()
 
-	fmt.Fprintf(w, content)
+	// return response with error checking
+	_, err := fmt.Fprintf(w, content)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+	}
+}
+
+// This API will delete all messages sent from the data file
+func flush(w http.ResponseWriter, req *http.Request) {
+	// use mutex to prevent race condition
+	mutex.Lock()
+	err := flushFileContent(getFilePath(dataFileName))
+	mutex.Unlock()
+
+	// error checking after operation
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	// return response with error checking
+	_, err = fmt.Fprintf(w, "Data cleaned! Use /send API to send message.")
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+	}
 }
 
 // This API will send the HTML page for client
@@ -44,5 +80,9 @@ func get(w http.ResponseWriter, req *http.Request) {
 func try(w http.ResponseWriter, req *http.Request) {
 	content := readFileContent(getFilePath(htmlFileName))
 
-	fmt.Fprintf(w, content)
+	// return response with error checking
+	_, err := fmt.Fprintf(w, content)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+	}
 }
